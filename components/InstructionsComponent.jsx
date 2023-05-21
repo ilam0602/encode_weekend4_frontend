e2fdce5e2258910e19c97a1e09ab649857ef12c7
ilam0602 <isaac.lam@live.com>
2023-05-21 11:58:16 -0700

    second commit

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -1,8 +1,9 @@
 import styles from "../styles/InstructionsComponent.module.css";
 import Router, { useRouter } from "next/router";
-import {useSigner, useNetwork, useBalance,useContract,useContractWrite,usePrepareContractWrite} from 'wagmi';
+import {useSigner, useNetwork, useBalance,useContract,useContractWrite,usePrepareContractWrite,useContractRead} from 'wagmi';
 import {useState,useEffect} from 'react'
 import * as tokenJson from '../assets/MyERC20Votes.json'
+import * as ballotJson from '../assets/TokenizedBallot.json'
 export default function InstructionsComponent() {
 	const router = useRouter();
 	return (
@@ -14,6 +15,8 @@ export default function InstructionsComponent() {
 			</header>
 			<RequestTokens></RequestTokens>
 			<Delegate></Delegate>
+			{/* <Query></Query> */}
+			<Proposal></Proposal>
 		</div>
 	);
 }
@@ -55,50 +58,93 @@ function Delegate(){
 	const {data:signer} = useSigner();
 
 	const [txData,setTxData] = useState(null);
-	const [isLoading,setLoading] = useState(false);
+	// const [isLoading,setLoading] = useState(false);
 	// console.log(tokenJson.abi);
-	const {config} = usePrepareContractWrite({
+	const {data,isLoading,isSuccess,write} = useContractWrite({
 		address: '0xBd8D607d146aC196051280d4Cb7952D95dA6F3bd',
 		abi: tokenJson.abi,
 		functionName: 'delegate',
-		inputs: [],
-		args: '0xBB923B99A0067e8ae37533898B849d67B8f3268e'
-
+		args: ['0xBB923B99A0067e8ae37533898B849d67B8f3268e']
 	});
+
 	if(signer){
 		console.log(signer.address);
 	}
 	
-	const { delegate } = useContractWrite(config);
+	// const { delegate } = useContractWrite(config);
 
-
-
-	if(txData) return (
-		<div>
-			<p>Transaction Completed!</p>
-			<a href ={"https://goerli.etherscan.io/tx/"+txData.hash} target = "_blank"> {txData.hash}</a>
-		</div>
-	)
-	if(isLoading) return <p> delegating...</p>;
 	return(
 		<div>
-			<button disabled = {!delegate} onClick = {() => delegate?.()}> Self Delegate Tokens</button>
-			{/* <button onClick = {() => delegate?.()}> Self Delegate Tokens</button> */}
+			<button onClick={() => write()}>Feed</button>
+			{isLoading && <div>Check Wallet</div>}
+			{isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
 		</div>
 	)
 }
-function delegate(contract,signer,setLoading,setTxData){
-	setLoading(true);
-	// const {data:contract} = useContractWrite({
-	// 	addressOrName: '0xBd8D607d146aC196051280d4Cb7952D95dA6F3bd',
-	// 	contractInterface: tokenJson.abi,
-	// },'delegate',
-	// {
-	// 	args:[signer.address]
-	// }
-	// )
-	// contract.delegate(signer.address)
-	setLoading(false);
-	setTxData(data);
+
+function Query(){
+	const {data:signer} = useSigner();
+
+	const [txData,setTxData] = useState(null);
+	// const [isLoading,setLoading] = useState(false);
+	// console.log(tokenJson.abi);
+	const {data,isLoading,isSuccess,refetch} = useContractRead({
+		address: '0x8581C8307D0F2D79b235c64F87C01C4C7aabb94d',
+		abi: ballotJson.abi,
+		functionName: 'winningProposal',
+		args: []
+	});
+
+	if(signer){
+		console.log(signer.address);
+	}
+	console.log(data);
+	
+	// const { delegate } = useContractWrite(config);
+
+	return(
+		<div>
+			<button onClick={() => refetch()}>Check Winner</button>
+			{isLoading && <div>Checking Winner</div>}
+			{isSuccess && <div>Winner: {JSON.stringify(data)}</div>}
+		</div>
+	)
+
 }
+
+function Proposal(){
+	const {data:signer} = useSigner();
+
+	const [txData,setTxData] = useState(null);
+	// const [isLoading,setLoading] = useState(false);
+	// console.log(tokenJson.abi);
+	const {data,isLoading,isSuccess,refetch} = useContractRead({
+		address: '0x8581C8307D0F2D79b235c64F87C01C4C7aabb94d',
+		abi: ballotJson.abi,
+		functionName: 'proposals',
+		args: [0]
+	});
+
+	if(signer){
+		console.log(signer.address);
+	}
+	console.log(data);
+	
+	// const { delegate } = useContractWrite(config);
+
+	return(
+		<div>
+			{isLoading && <div>Checking Proposal 0 </div>}
+			{isSuccess && <div>Proposal 0: {JSON.stringify(data)}</div>}
+		</div>
+	)
+
+}
+
+function Vote(){
+
+}
+
+
+
 
